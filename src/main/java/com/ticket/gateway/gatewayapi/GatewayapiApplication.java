a779bfc5b0a649211a769348e8e57d1dafb4b520
KHANH <khanh@KHANHs-MacBook-Pro.local>
2024-03-17 22:52:21 +0700

    add filter and auth for api gateway

--- src/main/java/com/ticket/gateway/gatewayapi/GatewayapiApplication.java
+++ src/main/java/com/ticket/gateway/gatewayapi/GatewayapiApplication.java
@@ -2,10 +2,10 @@ package com.ticket.gateway.gatewayapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableWebFluxSecurity
+@EnableDiscoveryClient
 public class GatewayapiApplication {
 
 	public static void main(String[] args) {
